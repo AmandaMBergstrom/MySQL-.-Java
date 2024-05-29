92faa492d77c8e2ec56dedee8979e5190b42a23e
Amanda Bergstrom <Amassaro777@gmail.com>
2024-05-29 10:37:57 -0700

    updating for week 10

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -17,11 +17,13 @@ import java.util.Scanner;
 public class ProjectsApp {
 	private Scanner scanner = new Scanner(System.in);
 	private ProjectService projectService = new ProjectService();
+	private Project curProject;
 	
 	//@formatter:off
 		private List<String> operations = List.of(
 				"1) Add a project",
-				"2)List projects"
+				"2) List projects",
+				"3) Select a project"
 				);
 	//formatter:on
 	
@@ -31,7 +33,7 @@ public class ProjectsApp {
 	private void processUserSelections() {
 		boolean done = false;
 		while(!done) {
-			try {
+			try { 
 				int selection = getUserSelection();
 				
 				switch(selection) {
@@ -45,6 +47,9 @@ public class ProjectsApp {
 				case 2:
 					listProjects();
 					break;
+				case 3:
+					selectProject();
+					break;
 					
 					default:
 						System.out.println("\n" + selection + " is not a valid selection. Try again.");
@@ -56,8 +61,18 @@ public class ProjectsApp {
 		}
 	}
 	
+	private void selectProject() {
+		listProjects();
+		Integer projectId = getIntInput("Enter a project ID to select a project");
+		
+		curProject = null;
+		curProject = projectService.fetchProjectById(projectId);
+		
+		
+	}
 	private void listProjects() {
 		List<Project> projects = projectService.fetchAllProjects();
+		
 		System.out.println("\nProjects:");
 		
 		projects.forEach(project -> System.out.println("  " + project.getProjectId() + ": " + project.getProjectName()));
@@ -128,6 +143,13 @@ public class ProjectsApp {
 	}
 	private void printOperations() {
 		System.out.println("\nThese are the available selections. Press the Enter key to quit:");
+		
 		operations.forEach(line -> System.out.println(" " + line));
+		
+		if(Objects.isNull(curProject)) {
+			System.out.println("\nYou are not working with a project.");
+		} else {
+			System.out.println("\nYou are working with project: " + curProject);
+		}
 	}
 	}	
--- src/main/java/projects/service/ProjectService.java
+++ src/main/java/projects/service/ProjectService.java
@@ -1,16 +1,30 @@
 package projects.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+
+import projects.dao.ProjectDao;
 import projects.entity.Project;
 
-public class ProjectService {
+public class ProjectService {	
+	private ProjectDao projectDao = new ProjectDao();
 
-	public List<Project> fetchAllProjects() {
-		return null;
-	}
 
 	public Project addProject(Project project) {
-		return null;
+		return projectDao.insertProject(project);
 	}
 
+
+	public List<Project> fetchAllProjects() {
+		return projectDao.fetchAllProjects();
+	}
+	
+	
+	public Project fetchProjectById(Integer projectId) {
+		return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException(
+			"Project with project ID = " + projectId + " does not exist."));
+	}
 }
+
+
+
